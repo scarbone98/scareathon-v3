354d8e8ea005b5722ec9f9bc0319a530273476e4
Samuel Carbone <scarbone1998@gmail.com>
2024-09-23 15:31:05 -0400

    added endpoint to get posts

--- server/index.js
+++ server/index.js
@@ -57,6 +57,21 @@ fastify.get('/calendar', async (request, reply) => {
     return { data: data }
 })
 
+fastify.get('/posts', async (request, reply) => {
+    try {
+        const response = await fetch(`${process.env.STRAPI_URL}/api/posts`, {
+            headers: {
+                'Authorization': `Bearer ${process.env.STRAPI_TOKEN}`
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        fastify.log.error(error);
+        reply.code(500).send({ error: 'Internal Server Error' });
+    }
+});
+
 // Run the server!
 const start = async () => {
     try {
