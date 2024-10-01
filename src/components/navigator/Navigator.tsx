60fd234474757132aa5105ce0c66a1a7c7f0b0c2
Samuel Carbone <scarbone1998@gmail.com>
2024-10-01 00:08:21 -0400

    better styling

--- src/components/navigator/Navigator.tsx
+++ src/components/navigator/Navigator.tsx
@@ -72,7 +72,7 @@ export const Navigator = () => {
             <li key={item.name}>
               <Link
                 to={item.path}
-                className="px-4 py-2 text-4xl hover:bg-gray-100 rounded"
+                className="px-4 py-2 text-4xl hover:bg-gray-100 rounded tracking-wide"
                 style={{
                   color: getColor(item),
                 }}
@@ -126,7 +126,7 @@ export const Navigator = () => {
                   <Link
                     to={item.path}
                     onClick={() => setIsOpen(false)}
-                    className={`block px-4 py-3 text-lg hover:text-white transition-colors duration-200`}
+                    className={`block px-4 py-3 text-2xl hover:text-white transition-colors duration-200 tracking-[0.15em]`}
                     style={{
                       color: getColor(item),
                     }}
